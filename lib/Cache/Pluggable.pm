bd39f0ae26af6e8b2526be51b6d4cafd0e71c869
Keiji, Yoshimi <walf443@gmail.com>
2011-03-16 21:00:27 +0900

    initial commit

---
+++ lib/Cache/Pluggable.pm
@@ -0,0 +1,33 @@
+package Cache::Pluggable;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cache::Pluggable -
+
+=head1 SYNOPSIS
+
+  use Cache::Pluggable;
+
+=head1 DESCRIPTION
+
+Cache::Pluggable is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
