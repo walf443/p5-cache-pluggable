9c4cb9070271d553db93eb0fb83263356add798b
Keiji, Yoshimi <walf443@gmail.com>
2011-03-17 21:20:41 +0900

    added doc to Plugin::Cache.

--- lib/Cache/Pluggable/Plugin/Namespace.pm
+++ lib/Cache/Pluggable/Plugin/Namespace.pm
@@ -19,4 +19,53 @@ no Mouse::Role;
 
 __END__
 
+=head1 NAME
+
+Cache::Pluggable::Role::Namespace - added namespace to key.
+
+=head1 SYNOPSIS
+
+  package Proj::Cache;
+  use Mouse;
+  extends 'Cache::Pluggable';
+
+  with qw(Cache::Pluggable::Plugin::Namespace);
+
+  __PACKAGE__->meta->make_immutable();
+
+  package main;
+  use strict;
+  use warnings;
+  use Cache::Memcached::Fast;
+
+  my $cache = Proj::Cache->new(
+    namespace => "proj",
+    cache => Cache::Memcached::Fast->new(
+        servers => [{ address => 'localhost:11211', weight => 1 }],
+        namespace => 'proj:',
+    ),
+  );
+  my $value = $cache->get($key); # "GET proj:$key"
+  $cache->set(foo => $value, $expire); # "SET proj:$key $value"
+
+=head1 DESCRIPTION
+
+This plugin always append namespace to key.
+
+It's useful to share cache storages between many projects.
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
++<Cache::Pluggable>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
